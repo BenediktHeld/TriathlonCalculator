3e4bf8f6c52a98347796015c8f78e15ebaa5c37a
BenediktHeld <benedikt.bh75@t-online.de>
2024-12-16 14:34:37 -0500

    Update PaceCalculator.py

--- PaceCalculator.py
+++ PaceCalculator.py
@@ -1,30 +1,38 @@
 
 class PaceCalculator:
     def __init__(self):
+        #Zonen Faktoren
         self.Zone1Faktor = 1.35
         self.Zone2Faktor = 1.22
         self.Zone3Faktor = 1.09
         self.Zone4Faktor = 1.02
         self.Zone5Faktor = 0.95
 
+        #Zone Pace set auf 0 am Anfang
+        self.Zone1Pace = 0
+        self.Zone2Pace = 0
+        self.Zone3Pace = 0
+        self.Zone4Pace = 0
+        self.Zone5Pace = 0
+
     def pace_zonen(self,minuten,sekunden):
 
         gesamt_sekunden = minuten * 60 + sekunden
 
-        # Berechnung der Pace-Zonen in Sekunden pro Kilometer
-        zonen = {
-            "Zone 1 (Erholung)": gesamt_sekunden * self.Zone1Faktor,
-            "Zone 2 (Grundlagenausdauer)": gesamt_sekunden * self.Zone2Faktor,
-            "Zone 3 (Tempo)": gesamt_sekunden * self.Zone3Faktor,
-            "Zone 4 (Laktat-Akkumulation)": gesamt_sekunden * self.Zone4Faktor,
-            "Zone 5 (VO2max)": gesamt_sekunden * self.Zone5Faktor
-        }
+        self.Zone1Pace = gesamt_sekunden * self.Zone1Faktor
+        self.Zone2Pace = gesamt_sekunden * self.Zone2Faktor
+        self.Zone3Pace = gesamt_sekunden * self.Zone3Faktor
+        self.Zone4Pace = gesamt_sekunden * self.Zone4Faktor
+        self.Zone5Pace = gesamt_sekunden * self.Zone5Faktor
 
-        # Ausgabe der Zonen in Minuten:Sekunden Format
-        for zone, pace in zonen.items():
-            minuten = int(pace // 60)
-            sekunden = int(pace % 60)
-            print(f"{zone}: {minuten}:{sekunden:02d}/km")
+        self.printPace()
+
+    def printPace(self):
+        print(f"Zone 1 (Erholung): {int((self.Zone1Pace)/60)}:{int((self.Zone1Pace%60))}/km")
+        print(f"Zone 2 (Grundlagenausdauer):  {int((self.Zone2Pace)/60)}:{int((self.Zone2Pace%60))}/km")
+        print(f"Zone 3 (Tempo):  {int((self.Zone3Pace)/60)}:{int((self.Zone3Pace%60))}/km")
+        print(f"Zone 4 (Laktat-Akkumulation):  {int((self.Zone4Pace)/60)}:{int((self.Zone4Pace%60))}/km")
+        print(f"Zone 5 (VO2max):  {int((self.Zone5Pace)/60)}:{int((self.Zone5Pace%60))}/km")
 
     def inputPace(self):
         print("---------------------------------")
